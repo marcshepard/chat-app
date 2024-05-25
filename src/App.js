a5a610062752cc2562109206d73fbae12f9ca1d7
marcshepard <marcshepard@outlook.com>
2024-05-25 16:04:08 -0700

    Added back the chat feature

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@
  */
 
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { AuthenticatedTemplate, UnauthenticatedTemplate } from "@azure/msal-react";
-import { SignInButton, SignOutButton } from "./MsalSignin";
-import { NavigationBar, DropDownMenu } from "./Controls";
+import { SignInButton, SignOutButton, useAuthToken } from "./MsalSignin";
+import { ScrollableTextBox, TextInput, NavigationBar, DropDownMenu } from "./Controls";
 
 // Configuration variables
 const APP_NAME = "Chat App";                          // Branding
 const LOGO = `${process.env.PUBLIC_URL}/logo.png`;    // Branding
+//const BASE_URL = "http://localhost:5000"
+const BASE_URL = "https://chat-ws.azurewebsites.net/";
+const CHAT_URL = BASE_URL + "/chat";
 
 const CHATBOTS = [                                    // Chatbot configurations
   {
@@ -39,6 +42,11 @@ const CHATBOTS = [                                    // Chatbot configurations
 
 /**
  * SignInHeader: A header for the sign-in page, with app logo, name, and a sign-in button
+ * 
+ * @param {string} logo - The URL of the app logo
+ * @param {string} appName - The name of the app
+ * 
+ * @returns {JSX.Element} The app header
  */
 function SignInHeader({logo, appName}) {
   return (
@@ -75,6 +83,172 @@ function AppHeader({logo, appName}) {
   );
 }
 
+/**
+  * getChatResponse - Send a set of chat messages to the server and return the response
+  *
+  * @param {Object[]} messages - The array of messages to send to the server
+  * @param {number} forSession - The session number for which the messages are being sent
+  * @param {authToken} authToken - The authentication token for the user
+  * 
+  * @returns {Promise} that resolves to the tuple [server response, forSession]
+  */
+async function getChatResponse(messages, forSession, authToken) {
+  console.log("getChatResponse sending messages to server: ", messages);
+
+  try {
+    const response = await fetch(CHAT_URL, {
+      method: "POST",
+      headers: {
+          "Authorization": "Bearer " + authToken,
+          "Content-Type": "application/json",
+      },
+      body: JSON.stringify(messages),
+    })
+    const responseText = await response.text();
+    if (!response.ok) {
+      throw new Error("HTTP error " + response.status + " " + responseText);
+    }
+    return [responseText, forSession];
+  } catch (error) {
+    console.error("Error:", error.messages);
+    return ["Sorry, but I'm having some issues right now (" + error.message + ")", forSession];
+  }
+}
+
+/**
+ * ScrollableChat - A scrollable text box showing the user and bot messages
+ * @param {Object[]} chatData - the (role, content) array of messages to display
+ * @param {string} userName - the name to use for the user role
+ * @param {string} botName - the name to use for the bot role
+ * 
+ * @returns {JSX.Element} The rendered React component for the scrollable chat, scrolled to the end
+ */
+function ScrollableChat({chatData, userName, botName}) {
+  // Function to format an array of (name, text)
+  const formatChatData = (chatData) => {
+    return chatData.map((dict, index) => (
+      <div key={index}>
+        <p><strong>{dict["role"] === "user" ? userName : botName}</strong>: {dict["content"]}</p>
+      </div>
+    ));
+  };
+
+  const chatText = formatChatData(chatData.slice(1)); // Skip the system message
+
+  return (
+    <ScrollableTextBox text={chatText} />
+  );
+}
+
+/**
+  * Chat - the main control consisting of a scrollable list of messages and a text input box
+  * 
+  * @param {dict} chatBot - the configuration of the currently selected chatbot
+  * @param {number} session - the current session number; chat is reset when this number changes (or chatbot changes)
+  * @param {boolean} sessionIsActive - flag to indicate if the chat session is active; used to trigger a summary on session end
+  * 
+  * @returns {JSX.Element} The rendered React component for the chat control
+  */
+function Chat({chatBot, session, sessionIsActive}) {
+  // State that is reset whenever a new session is started
+  const [messages, setMessages] = useState([]);
+  const summarized = useRef(false);
+  const aquireAuthToken = useAuthToken();   // Get a function to squire the authentication token
+  const awaitingResponse = useRef(false);   // Flag to track if a response from the server is pending
+
+  console.log("In chat with ", chatBot["name"], ", session " + session, ", isActive=", sessionIsActive, "summarized=", summarized.current);
+
+  // Reset state when a new session is started or a new chatbot is selected
+  useEffect(() => {
+    console.log("Starting new session: ", session)
+    setMessages ([
+      {"role": "system", "content": chatBot["context"] + ". Your name is " + chatBot["name"] + " and your personality is " + chatBot["personality"] + "."},
+      {"role": "assistant", "content": chatBot["greeting"]},
+    ]);
+    summarized.current = false;
+    awaitingResponse.current = false;
+  }, [session, chatBot]);
+
+  // When awaitingResponse is set to true, get an asynchronous response from the server and append it to the messages
+  useEffect(() => {
+    console.log("Awaiting response: ", awaitingResponse.current);
+    if (!awaitingResponse.current) {
+      console.log("Not awaiting response after all...");
+      return;
+    }
+
+    async function getResponse() {
+      console.log("Getting authToken for session ", session);
+      const authToken = await aquireAuthToken();
+      console.log("Calling server");
+      const [response, forSession] = await getChatResponse(messages, session, authToken);
+      awaitingResponse.current = false;
+      if (session !== forSession) {
+        console.log("Ignoring response for old session ", forSession);
+        return;
+      }
+      console.log("Got response: ", response);
+      setMessages(messages => [...messages, {"role": "assistant", "content" : response}]);
+    }
+
+    getResponse();
+  }, [aquireAuthToken, awaitingResponse, messages, session]);
+
+  // When the session ends, get an asynchronous summary from the server
+  useEffect(() => {
+    // Nothing to do if already summarized or the session is still active
+    if (sessionIsActive || summarized.current)
+      return;
+    
+    console.log("Summarizing chat");
+    summarized.current = true;
+
+    // Get a text version of the current chat
+    const chatText = messages.map((dict) => {
+      if (dict["role"] === "system") {
+        return "";
+      } else {
+        return dict["role"] + ": " + dict["content"] + "\n";
+      }
+    }).join("\n");
+    console.log("Chat text: ", chatText);
+    
+    // Ask the chatbot to summarize it
+    const summaryMessages = [
+      {"role": "system", "content": chatBot["summarize_context"]},
+      {"role": "user", "content": chatBot["summarize_prompt"] + "\n" + chatText},
+    ];
+
+    aquireAuthToken().then((authToken) => {
+      getChatResponse(summaryMessages, session, authToken).then((response) => {
+      setMessages(messages => [...messages, {"role": "assistant", "content" : response[0]}]);
+      });
+    });
+  }, [sessionIsActive, summarized, messages, session, chatBot, aquireAuthToken]);
+
+  // When the user types something, append it to the messages and await a response
+  function handleUserInput(text) {
+    // Append the users input to the messages
+    setMessages(messages => [...messages, {"role": "user", "content" : text}])
+
+    // Call getChatResponse asynchronously with the updated set of messages
+    awaitingResponse.current = true;
+  }
+
+  return (
+    <div>
+      <p><b>{chatBot["name"]}</b> is {chatBot["description"]}.</p>
+      <ScrollableChat chatData={messages} userName="You" botName={chatBot["name"]} />
+      <TextInput handleInput={handleUserInput} placeholder="Type your message here" buttonText="Send" fullwidth={true} disabled={!sessionIsActive}/>
+    </div>
+  );
+}
+
+/**
+ * App: The main component for the chat app react front-end
+ * 
+ * @returns {JSX.Element} The rendered React component for the chat app
+ */
 function App() {
   // App configuration info; available bots and nav buttons
   const botTypes = CHATBOTS.map((bot) => bot["type"]);
@@ -121,7 +295,7 @@ function App() {
           <DropDownMenu name="Chat type" options={botTypes} handleSelect={handleBotSelection} />
           <br />
           <NavigationBar buttons={buttonNames} descriptions={buttonDescriptions} selectedButton={button} handleClick={handleButtonClick} />
-          <p>Session is active: {sessionIsActive.toString()}</p>
+          <Chat chatBot={chatBot} session={session} sessionIsActive={sessionIsActive}/>
         </AuthenticatedTemplate>
 
         <UnauthenticatedTemplate>
--- src/Controls.js
+++ src/Controls.js
@@ -5,6 +5,60 @@
  */
 
 import React from 'react';
+import { useState } from 'react';
+
+/**
+ * TextInput - Get text input from the user via text box + button.
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {Function} props.handleInput - Callback function to handle the input text.
+ * @param {string} props.placeholder - The placeholder text for the input box.
+ * @param {string} props.buttonText - The text to display on the button.
+ * @param {boolean} [props.fullwidth=false] - Whether the input box should take full width.
+ * @param {boolean} [props.disabled=false] - Whether the input box and button should be disabled.
+ *
+ * @returns {JSX.Element} The rendered React component for text input.
+ */
+export function TextInput({ handleInput, placeholder, buttonText, fullwidth = false, disabled = false }) {
+  const [text, setText] = useState("");
+
+  function handleClick() {
+    handleInput(text);
+    setText("");
+  }
+
+  return (
+    <div style={{ display: "flex", width: fullwidth ? "100%" : undefined }}>
+      <input
+        disabled={disabled}
+        type="text"
+        placeholder={placeholder}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        onKeyDown={(e) => { if (e.key === "Enter") handleClick() }}
+        style={{ flex: fullwidth ? "1" : undefined, fontSize: "1.25em" }}
+      />
+      <button disabled={disabled} onClick={handleClick}>{buttonText}</button>
+    </div>
+  );
+}
+
+/**
+ * ScrollableTextBox - A scrollable text box, scrolled to the end.
+ *
+ * @param {Object} props - The properties passed to the component.
+ * @param {string} props.text - The text to display in the scrollable text box.
+ *
+ * @returns {JSX.Element} The rendered React component for scrollable text box.
+ */
+export function ScrollableTextBox({ text }) {
+  return (
+    <div style={{ height: "60vh", overflowY: "scroll", border: "1px solid black", padding: "10px" }}>
+      {text}
+      <div ref={(el) => { el && el.scrollIntoView({ behavior: "smooth" }); }}></div>
+    </div>
+  );
+}
 
 /**
  * NavigationBar - A horizontal set of navigation buttons with descriptions to be shown on hover.
@@ -25,7 +79,8 @@ export function NavigationBar({ buttons, descriptions, selectedButton, handleCli
   return (
     <div style={{ display: "flex", justifyContent: "space-between", alignItems: "center" }}>
       {buttons.map((button, index) => (
-        <button key={index} onClick={() => handleClick(button)} title={descriptions[index]} style={{ width: width + "%", maxWidth: max_width }} className={selectedButton === button ? "Selected" : ""}>
+        <button key={index} onClick={() => handleClick(button)} title={descriptions[index]}
+              style={{ width: width + "%", maxWidth: max_width, backgroundColor: selectedButton === button ? "#f5f5f5" : undefined }} >
           {button}
         </button>
       ))}
