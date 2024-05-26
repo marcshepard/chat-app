ec75765e866c13b77094ac625fedb7101b1318ef
marcshepard <marcshepard@outlook.com>
2024-05-26 13:29:15 -0700

    Prompt on each signin so the user can switch accounts if they want to.

--- src/msalConfig.js
+++ src/msalConfig.js
@@ -56,7 +56,8 @@ export const msalConfig = {
  * https://docs.microsoft.com/en-us/azure/active-directory/develop/v2-permissions-and-consent#openid-connect-scopes
  */
 export const loginRequest = {
-    scopes: ["openid", "profile", "email"]
+    scopes: ["openid", "profile", "email"],
+    prompt: "select_account"
 };
 
 /*
